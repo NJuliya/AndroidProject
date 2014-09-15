a246079b434d9fbba15be2b9fc401f94fc5636d2
NJuliya <nagumanova_yulya@mail.ru>
2014-09-15 10:52:59 +0400

    Update and rename Fragment2.java to FragTh.java

--- app/src/main/java/com/samples/yulya/phcollage/FragTh.java
+++ app/src/main/java/com/samples/yulya/phcollage/FragTh.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment2 extends Fragment {
+public class FragTh extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment2, null);
