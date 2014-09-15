861fc3a028e1f6e849b8a628c0ec33db40310d8b
NJuliya <nagumanova_yulya@mail.ru>
2014-09-15 10:53:26 +0400

    Update and rename Fragment3.java to FragF.java

--- app/src/main/java/com/samples/yulya/phcollage/FragF.java
+++ app/src/main/java/com/samples/yulya/phcollage/FragF.java
@@ -6,7 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Fragment3 extends Fragment {
+public class FragF extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment3, null);
